dfeae587cfdf329fbab0e28a0b4ba23587af2e44
Johannes Jansson <johannes.e.jansson@gmail.com>
2020-10-29 21:05:45 +0100

    Add chromium and bundix to linux

--- nix/linux.nix
+++ nix/linux.nix
@@ -3,11 +3,15 @@
 {
   home.packages = [
     pkgs.firefox
+    pkgs.chromium
     pkgs.kitty
     pkgs.vlc
     pkgs.awscli
     pkgs.dropbox
     pkgs.pg_top
     pkgs.gparted
+
+    # for generating gem nix
+    pkgs.bundix
   ];
 }
